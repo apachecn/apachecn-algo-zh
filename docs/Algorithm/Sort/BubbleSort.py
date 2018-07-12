29a8c7e61a3d15bdc43da34a7e2fda055fa9bbaa
jiangzhonglian <jiang-s@163.com>
2018-07-12 12:56:26 +0800

    更新 冒泡排序 的使用说明

--- docs/Algorithm/Sort/BubbleSort.py
+++ docs/Algorithm/Sort/BubbleSort.py
@@ -2,17 +2,19 @@
 
 
 # 冒泡排序
-# 1. 外层循环负责设置冒泡排序进行的次数
-# 2. 内层循环负责前后两两比较，求最大值放到最后
+# 1. 外层循环负责帮忙递减内存循环的次数【len-1, 1】
+# 2. 内层循环负责前后两两比较, index 的取值范围【0, len-2】 len-1-i 次，求最大值放到最后
 def bubble_sort(nums):
-    for i in range(len(nums)-1):
-        for j in range(len(nums)-i-1):  # ｊ为列表下标
+    # [1, len-1]
+    for i in range(1, len(nums)-1):
+        # [0, len-1-i]
+        for j in range(len(nums)-i):  # ｊ为列表下标
             if nums[j] > nums[j+1]:
                 nums[j], nums[j+1] = nums[j+1], nums[j]
     return nums
 
 
 if __name__ == "__main__":
-    nums = [5, 1, 9, 3, 2, 7]
+    nums = [2, 6, 8, 5, 1, 4, 9, 3, 7]
     bubble_sort(nums)
     print(nums)
