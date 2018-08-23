d8b6cf7c9dab453ea8e980287abec2cac3e93c99
Keqi Huang <keqih@smu.edu>
2018-08-23 18:35:24 -0500

    Update InsertSort.py

--- src/py3.x/sort/InsertSort.py
+++ src/py3.x/sort/InsertSort.py
@@ -8,18 +8,19 @@
 from __future__ import print_function
 
 
-def insert_sort(l):
-    # 循环  除第一个数字组成的有序数组 以外的数字
-    for i in range(1, len(l)):
-        # 每一个数字，依次和有序数组进行比较
-        print(l[:i])
-        for j in range(len(l[:i])):
-            if l[i] < l[j]:
-                l[i], l[j] = l[j], l[i]
+def insertionSort(nums):
+    if not nums or len(nums) < 2:
+        return nums
+
+    for i in range(1, len(nums)):
+        for j in range(i):
+            if nums[i] < nums[j]:
+                nums[i], nums[j] = nums[j], nums[i]
+    return nums
 
 
 if __name__ == "__main__":
-    l = [5, 1, 9, 3, 2, 7]
-    print(l)
-    insert_sort(l)
-    print("result: " + str(l))
+    nums = [5, 1, 9, 3, 2, 7]
+    print('input: ', nums)
+    nums = insertionSort(nums)
+    print("result: ", nums)
