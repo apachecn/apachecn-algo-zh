78976e7c0d64686faa30aa045c21e655b3213c37
cclauss <cclauss@me.com>
2019-02-19 04:43:22 +0100

    Simplify InversePair by using sorted()

--- docs/剑指offer/Python/35-数组中的逆序对.py
+++ docs/剑指offer/Python/35-数组中的逆序对.py
@@ -5,14 +5,10 @@ class Solution:
     def InversePairs(self, data):
         # write code here
         count = 0
-        copy = []
-        for i in data:
-            copy.append(i)
-        copy.sort()
-         
-        for i in range(len(copy)):
-            count += data.index(copy[i])
-            data.remove(copy[i])
+
+        for item in sorted(data):
+            count += data.index(item)
+            data.remove(item)
              
         return count%1000000007
     
